0cae445bfb22fdce467b498b0b1b208835e26bd1
Jackson Chen <jacksonchen171@gmail.com>
2020-06-04 17:11:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   counter = 0
   newarray = []
   while array[counter] do
-    newarray << (array[counter]**)
+    newarray << array[counter]*array[counter]
     counter += 1
   end
   newarray
